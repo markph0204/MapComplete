870e0b4c41406716b6245ae75e4d428f90ac8318
heltonbiker <heltonbiker@gmail.com>
2016-03-27 21:19:46 -0300

    Update SrtmSources.py

--- SrtmSources.py
+++ SrtmSources.py
@@ -2,20 +2,31 @@ import urllib
 import zipfile
 from geofunctions import *
 
-baseurl = 'http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/South_America/{0}{1:02}{2}{3:03}.hgt.zip'
+ddsUrl = 'http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/South_America/'
+ddsFname = '{0}{1:02}{2}{3:03}.hgt.zip'
 
-def demtilefromlonlat(coords):
-    xi, yi = coords.astype(int)
+# dropprUrl = 'http://droppr.org/srtm/v4.1/6_5x5_TIFs/'  same fname as cgiar
+cgiarUrl = "http://srtm.csi.cgiar.org/SRT-ZIP/SRTM_V41/SRTM_Data_GeoTiff/"
+
+srtmFname = 'srtm_{}_{}.zip'
+
+
+def cgiarTileFromLonLat(coords):
+    lon, lat = coords
+    xi = (180 + int(lon)) / 5
+    yi = (60 - int(lat)) / 5
+    tilename = nameformat.format(xi, yi)
+    link = baseurl + tilename
+    dest = './' + tilename
+    urllib.urlretrieve(link, dest)
+    return tilename
+
+def ddsTileFromLonLat(coords):
+    xi, yi = coords
     xstring = 'E' if xi > 0 else 'W'
     ystring = 'N' if yi > 0 else 'S'
-    filename = tileformat.format(ystring, abs(yi), xstring, abs(xi))
-    urllib.urlretrieve(baseurl+filename, cache_path+filename)
+    tilename = ddsFname.format(ystring, abs(yi), xstring, abs(xi))
+    link = baseurl + ddsFname
+    dest = './' + ddsFname
+    urllib.urlretrieve(link, cache_path+filename)
 
-
-coords = numpy.array(coordfromstring('-30, -50'))
-
-demtilefromlonlat(coords)
-
-
-
-dss = "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/South_America/
