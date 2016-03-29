e7cb597910be02fc29776362bead45c5434a9d7a
heltonbiker <heltonbiker@gmail.com>
2016-03-28 23:52:00 -0300

    Created menu bar with Open menu, opening a file dialog to choose a KML file, which it reads and prints to console

--- MapComplete.py
+++ MapComplete.py
@@ -17,10 +17,34 @@ class MapComplete(QApplication):
         window = QMainWindow(None)
         self.setApplicationName("MapComplete")
         window.setWindowTitle("MapComplete")
+
+        bar = window.menuBar()
+        _open = bar.addMenu("Open")
+        _open.addAction("KML from file...",self.KmlFromFile)
+        _open.addAction("KML from link...",self.KmlFromLink)
+
         mapView = MapView()
         window.setCentralWidget(mapView)
         window.show()
         sys.exit(self.exec_())
+
+    def KmlFromFile(self):
+        dlg = QFileDialog()
+        dlg.setFileMode(QFileDialog.AnyFile)
+        dlg.setFilter("KML files (*.kml)")
+        filenames = QStringList()
+
+        if dlg.exec_():
+            filenames = dlg.selectedFiles()
+            f = open(filenames[0], 'r')
+
+            with f:
+                data = f.read()
+                print data
+
+
+    def KmlFromLink(self):
+        pass
 
 
 if __name__ == '__main__':
